3bd7db888e70bc70a2a450d1c8daec8e844d0f70
= <hdubbs77@gmail.com>
2020-02-28 19:08:06 -0500

    added sign in and register

--- app/src/main/java/net/cit368/reminderapp/MainActivity.java
+++ app/src/main/java/net/cit368/reminderapp/MainActivity.java
@@ -1,17 +1,19 @@
 package net.cit368.reminderapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,14 +22,15 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    //AccountHandler accountHandler = new AccountHandler();
-    private static final int SIGNIN_CODE = 1;
-    private FirebaseUser user;
+    private static final int SIGNIN_CODE = 1, REGISTER_CODE = 2;
+    private FirebaseAuth auth;
+    public FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
         Button btnSignin = findViewById(R.id.btn_signin);
         Button btnRegister = findViewById(R.id.btn_register);
         btnSignin.setOnClickListener(new View.OnClickListener(){
@@ -37,11 +40,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         btnRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
             public void onClick(View v){
-
+                startActivityForResult(new Intent(MainActivity.this, RegisterActivity.class), REGISTER_CODE);
             }
         });
 
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        user = auth.getCurrentUser();
+        //the user can bypass sign in if they are already signed in
+        if(user != null){
+            //TODO redirect to tasks activity
+        }
     }
 
     public void createSigninIntent(){
@@ -57,10 +71,28 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 user = FirebaseAuth.getInstance().getCurrentUser();
                 //TODO redirect to tasks page
-                System.out.println("LOG IN SUCCESSFUL");
+                System.out.println("======================LOG IN SUCCESSFUL======================");
             } else {
-                //TODO handle failed signin
-                System.out.println("LOG IN FAILURE");
+                Toast.makeText(MainActivity.this, "Log In Failed", Toast.LENGTH_SHORT).show();
+                System.out.println("======================LOG IN FAILURE======================");
+            }
+        }else if(requestCode == REGISTER_CODE){
+            Bundle extras = data.getExtras();
+            if(resultCode == RESULT_OK){
+                auth.createUserWithEmailAndPassword(extras.getString("username"), extras.getString("password")).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            System.out.println("======================user created successfully======================");
+                            user = auth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "Log In Successful", Toast.LENGTH_SHORT).show();
+                            //TODO redirect to tasks activity
+                        }else{
+                            System.out.println("======================create account failed======================");
+                            Toast.makeText(MainActivity.this, "Could not create account", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
         }
     }
@@ -70,8 +102,8 @@ public class MainActivity extends AppCompatActivity {
                 .signOut(this)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     public void onComplete(@NonNull Task<Void> task) {
-                        //TODO rediriect user to login activity
                         System.out.println("LOGGED OUT");
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
                     }
                 });
     }
---
+++ app/src/main/java/net/cit368/reminderapp/RegisterActivity.java
@@ -0,0 +1,86 @@
+package net.cit368.reminderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * @author Hunter Dubbs
+ * @version 2/28/2020
+ * This class allows the user to input a username and password and validates them.
+ */
+public class RegisterActivity extends AppCompatActivity {
+
+    private String username, password;
+
+    /**
+     * Configure form fields and create submit button listener
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        final EditText txtUsername = findViewById(R.id.txtUsername);
+        final EditText txtPassword = findViewById(R.id.txtPassword);
+        Button btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = txtUsername.getText().toString();
+                password = txtPassword.getText().toString();
+                if(validate()) finish();
+            }
+        });
+    }
+
+    /**
+     * Validates the user email and password
+     * @return true if both fields validate successfully
+     */
+    public boolean validate(){
+        return validateUsername() && validatePassword();
+    }
+
+    /**
+     * Validates the user email to ensure it is not empty and is a valid email address
+     * @return true if the email field validates successfully
+     */
+    public boolean validateUsername(){
+        if(username != null && username != "" && Patterns.EMAIL_ADDRESS.matcher(username).matches()){
+            return true;
+        }
+        Toast.makeText(RegisterActivity.this, "Invalid Email", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    /**
+     * Validates the password field to ensure it is between 8 and 40 characters
+     * @return true if the email field validates successfully
+     */
+    public boolean validatePassword(){
+        if(password != null && (password.length() > 8 || password.length() < 40)){
+            return true;
+        }
+        Toast.makeText(RegisterActivity.this, "Invalid Password", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    /**
+     * Returns the validated fields back to MainActivity to actually generate the account.
+     */
+    @Override
+    public void finish(){
+        Intent result = new Intent();
+        result.putExtra("username", username);
+        result.putExtra("password", password);
+        setResult(RESULT_OK, result);
+        super.finish();
+    }
+}
